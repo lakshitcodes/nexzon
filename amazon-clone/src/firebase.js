83c13a82b7df09bb3766443382ca116479a3df6d
Lakshit Jain <jainlakshit849@gmail.com>
2024-06-22 21:56:59 +0530

    Navbar Finished

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBIPfhBnAZ8W3k7rzMSnDrJ7T1mrqcXQs",
+  authDomain: "lakshit-nexzon.firebaseapp.com",
+  projectId: "lakshit-nexzon",
+  storageBucket: "lakshit-nexzon.appspot.com",
+  messagingSenderId: "597917108064",
+  appId: "1:597917108064:web:8d64cf6e4a06e6d0a4c005",
+  measurementId: "G-7QSKPBFVCS",
+};
