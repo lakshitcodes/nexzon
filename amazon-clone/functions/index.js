a6a7ecbe6a5b2beeb2fbee5d87caa94f5c6c06fe
Lakshit Jain <jainlakshit849@gmail.com>
2024-06-29 16:17:21 +0530

    Changed Base URL from localhost to firebase

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,15 +1,14 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51PWcYDCUHs9kPndg93lojqtc5lQxGNZQ927TBqUxrWhfY8vGvsK5UXaO4nEqlniwfmJqNLNdfgcbpabOOthDtUdu00Tee99JSK"
-);
+const testKey="sk_test_51PWcYDCUHs9kPndg93lojqtc5lQxGNZQ927TBqUxrWhfY8vGvsK5UXaO4nEqlniwfmJqNLNdfgcbpabOOthDtUdu00Tee99JSK";
+const stripe = require("stripe")(testKey);
 
 // App Config
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // API Routes
@@ -19,16 +18,18 @@ app.get("/", (req, res) => {
 
 app.post("/payments/create", async (req, res) => {
   const total = req.query.total;
-
   console.log("Payment Request Received", total);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total,
-    currency: "inr",
-  });
 
-  res.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "inr",
+    });
+    res.status(201).send({clientSecret: paymentIntent.client_secret});
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    res.status(500).send({error: "Failed to create payment intent"});
+  }
 });
 
 // Listen Command
--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/lakshit-nexzon/us-central1/api",
+  baseURL: "https://us-central1-lakshit-nexzon.cloudfunctions.net/api",
+  // "http://127.0.0.1:5001/lakshit-nexzon/us-central1/api",
 });
 
 export default instance;
